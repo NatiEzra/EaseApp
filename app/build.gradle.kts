dbe0778ae8c90b1dad0faf6d67e800d0ed5a9c97
NatiEzra <128730088+NatiEzra@users.noreply.github.com>
2025-03-30 15:52:54 +0300

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,47 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-}
+    alias(libs.plugins.google.services)
+    id("androidx.navigation.safeargs.kotlin")
+    id ("kotlin-kapt")
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 
+
+}
+val room_version = "2.6.1"
+val secretsPropertiesFile = rootProject.file("secrets.properties")
+val secretsProperties =  Properties()
+secretsProperties.load( FileInputStream(secretsPropertiesFile))
 android {
-    namespace = "com.example.easeapp"
-    compileSdk = 34
+    namespace = "com.example.ease"
+    compileSdk = 35
+    buildFeatures {
+        buildConfig = true
+    }
+
+
+
 
     defaultConfig {
-        applicationId = "com.example.easeapp"
-        minSdk = 30
-        targetSdk = 34
+        applicationId = "com.example.ease"
+        minSdk = 26
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "CLOUD_NAME", "\"${project.properties["CLOUD_NAME"] ?: ""}\"")
+        buildConfigField("String", "API_KEY", "\"${project.properties["API_KEY"] ?: ""}\"")
+        buildConfigField("String", "API_SECRET", "\"${project.properties["API_SECRET"] ?: ""}\"")
+        buildConfigField("String", "GOOGLE_CLIENT_ID", secretsProperties["GOOGLE_CLIENT_ID"] as String)
+        buildConfigField("String", "Articles_Api", "\"${project.properties["Articles_Api"] ?: ""}\"")
+        buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${project.properties["GOOGLE_MAPS_API_KEY"] ?: ""}\"")
+
+
+
     }
 
     buildTypes {
@@ -34,7 +61,6 @@ android {
         jvmTarget = "11"
     }
 }
-
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -42,7 +68,40 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.auth)
+    implementation(libs.play.services.maps)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.room.common)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth.ktx)
+    implementation (libs.cloudinary.android)
+    implementation (libs.picasso)
+    implementation(libs.wasabeef.picasso.transformations)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation (libs.androidx.recyclerview)
+    implementation (libs.androidx.cardview)
+    implementation (libs.okhttp)
+    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+    implementation (libs.androidx.navigation.fragment.ktx.v276)
+    implementation (libs.androidx.navigation.ui.ktx.v276)
+    implementation (libs.androidx.navigation.fragment.ktx.v260)
+    implementation (libs.androidx.room.runtime)
+    kapt (libs.androidx.room.compiler)
+    implementation (libs.androidx.room.ktx)
+
+
+
+
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.services) apply false
+    id("androidx.navigation.safeargs") version "2.7.6" apply false
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
 }
