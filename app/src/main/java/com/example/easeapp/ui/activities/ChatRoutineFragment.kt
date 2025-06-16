7e5d8ac02fb19ba71be01bb1570c6598db6d4889
natanelezra <natanelezra@gmail.com>
2025-06-16 19:09:16 +0300

    add gender and phone to register

--- app/src/main/java/com/example/easeapp/ui/activities/ChatRoutineFragment.kt
+++ app/src/main/java/com/example/easeapp/ui/activities/ChatRoutineFragment.kt
@@ -13,6 +13,10 @@ import androidx.navigation.fragment.findNavController
 import com.example.ease.R
 import com.example.easeapp.repositories.AppointmentRepository
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 class ChatRoutineFragment : Fragment() {
     private lateinit var progressBar: ProgressBar
@@ -60,19 +64,44 @@ class ChatRoutineFragment : Fragment() {
             progressBar.visibility = View.VISIBLE
             lifecycleScope.launch {
                 try {
+                    var tooEarly: Boolean =false
                     val appointments = AppointmentRepository.shared.getUpcomingAppointmentForPatient(requireContext())
                     if (appointments != null) {
                         for (appointment in appointments) {
                             if (appointment.status == "confirmed") {
-                                val action = ChatRoutineFragmentDirections
-                                    .actionRoutineFragmentToMeetingChatFragment(appointment._id)
-                                findNavController().navigate(action)
-                                return@launch
+                                val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+                                formatter.timeZone = TimeZone.getTimeZone("UTC")
+                                val appointmentTime = formatter.parse(appointment.appointmentDate)
+                                val currentTime = Date()
+
+                                // Calculate time difference in minutes
+                                val diffInMillis = appointmentTime.time - currentTime.time
+                                val diffInMinutes = diffInMillis / (60 * 1000)
+                                if ( diffInMinutes<=10) {
+                                    val action = ChatRoutineFragmentDirections
+                                        .actionRoutineFragmentToMeetingChatFragment(appointment._id)
+                                    findNavController().navigate(action)
+                                    return@launch
+                                }
+                                else{
+                                    Toast.makeText(requireContext(), "You may join the meeting 10 minutes before it begins", Toast.LENGTH_SHORT).show()
+                                    progressBar.visibility = View.GONE
+                                    tooEarly=true
+                                }
+
+//                                val action = ChatRoutineFragmentDirections
+//                                    .actionRoutineFragmentToMeetingChatFragment(appointment._id)
+//                                findNavController().navigate(action)
+//                                return@launch
                             }
                         }
                     }
                     progressBar.visibility = View.GONE
-                    Toast.makeText(requireContext(), "No active meeting found", Toast.LENGTH_SHORT).show()
+                    if (!tooEarly)
+                    {
+                        Toast.makeText(requireContext(), "No active meeting found", Toast.LENGTH_SHORT).show()
+                    }
+
                 } catch (e: Exception) {
                     progressBar.visibility = View.GONE
                     Toast.makeText(requireContext(), "Error checking meeting status", Toast.LENGTH_SHORT).show()
